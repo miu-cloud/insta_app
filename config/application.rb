a18295d8403d2fcaf9dfbc46ae0596618508c29b
miu oohira <miu.000408@gmail.com>
2021-03-07 23:04:45 +0900

    見た目を整えた

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,16 @@
 require_relative 'boot'
-
 require 'rails/all'
-
 Bundler.require(*Rails.groups)
-
 module FcApp
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     config.load_defaults 5.2
     config.i18n.default_locale = :ja
+    
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
   end
 end
