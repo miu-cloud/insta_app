fa9e77a99281c82d76c1133797fb055faf4b3dae
miu oohira <miu.000408@gmail.com>
2021-03-07 16:57:52 +0900

    お気に入り機能とお気に入り一覧ページを作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,15 @@ class ApplicationController < ActionController::Base
     include SessionsHelper
     def authenticate_user
         if @current_user == nil
-           flash[:notice] = t('notice.login_needed')
+           flash[:notice] = t('notice.ログインしてください')
            redirect_to new_session_path
+        end
+    end
+
+    def fobid_login_user
+        if @current_user
+          flash[:notice]="ログインしています"
+          redirect_to user_show
         end
     end
 end
--- app/models/feed.rb
+++ app/models/feed.rb
@@ -3,4 +3,6 @@ class Feed < ApplicationRecord
     validates :image, presence: true
     validates :text, presence: true
     belongs_to :user
+    has_many :favorites, dependent: :destroy
+    has_many :favorite_users, through: :favorites, source: :user
 end
